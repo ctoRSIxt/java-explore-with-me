dff7bd7c420f33a8173f6e1a637b6c1de1fd3b37
Anton Prosekin <ayuprosekin@yandex.ru>
2023-04-07 00:17:54 +0800

    fix: remove unused import

--- stats/stats-client/src/main/java/ru/practicum/ewm/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/ewm/client/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.client;
 
-import org.apache.http.impl.bootstrap.HttpServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
